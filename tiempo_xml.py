3ad48144affaa126885fed9f87d0bc58ca32e955
carlosfdez91 <carlosfernandez16@gmail.com>
2014-03-06 21:32:16 +0100

    Depurado listo, aplicación funcional

--- tiempo_xml.py
+++ tiempo_xml.py
@@ -1,11 +1,10 @@
 #coding: utf-8
 import requests
-import json
 import webbrowser
 from lxml import etree
 from jinja2 import Template
 plantilla = open("plantilla.html","r")
-capitales = ['Almeria','Cadiz','Cordoba','Huelva','Jaen','Malaga','Sevilla','Ubeda']
+capitales = ['Almeria','Cadiz','Cordoba','Huelva','Jaen','Malaga','Sevilla']
 
 html = ''
 for linea in plantilla:
@@ -15,20 +14,18 @@ plantilla = Template(html)
 
 lista_temp_min = []
 lista_temp_max = []
-lista_redon = []
+lista_velocidad = []
 lista_direccion = []
 
 for capital in capitales:
-	valores = {'q': '%s' % capital,'mode': 'xml','units': 'metric','lang': 'es'}
-	resultado = requests.get('http://api.openweathermap.org/data/2.5/weather?',params=valores)
+	opciones = {'q': '%s' % capital,'mode': 'xml','units': 'metric','lang': 'es'}
+	resultado = requests.get('http://api.openweathermap.org/data/2.5/weather?',params=opciones)
 	raiz = etree.fromstring(resultado.text.encode("utf-8"))
 
-	city = raiz.find("city")
-	ciudad = city.attrib["name"]
-	temper_min = raiz.find("temperature")
-	temp_min = round(float(temper_min.attrib["min"]),1)
-	temper_max = raiz.find("temperature")
-	temp_max = round(float(temper_max.attrib["max"]),1)
+	minima = raiz.find("temperature")
+	temp_min = round(float(minima.attrib["min"]),1)
+	maxima = raiz.find("temperature")
+	temp_max = round(float(maxima.attrib["max"]),1)
 	velocidad = raiz.find("wind/speed")
 	speed = velocidad.attrib["value"]
 	orientacion = raiz.find("wind/direction")
@@ -36,11 +33,11 @@ for capital in capitales:
 	
 	lista_temp_min.append(temp_min)
 	lista_temp_max.append(temp_max)
-	lista_redon.append(speed)
+	lista_velocidad.append(speed)
 	lista_direccion.append(direccion)
 
 
-plantilla = plantilla.render(capitales=capitales,temp_min=lista_temp_min,temp_max=lista_temp_max,speed=lista_redon,direccion_viento=lista_direccion)
+plantilla = plantilla.render(capitales=capitales,temp_min=lista_temp_min,temp_max=lista_temp_max,speed=lista_velocidad,direccion_viento=lista_direccion)
 
 resultado=open('salida.html','w')
 resultado.write(plantilla)
