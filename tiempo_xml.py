3b588a59fe2163409bdef7c7706a8870f0b43038
carlosfdez91 <carlosfernandez16@gmail.com>
2014-03-06 12:42:08 +0100

    Aplicacion funcionando

--- tiempo_xml.py
+++ tiempo_xml.py
@@ -1,10 +1,11 @@
 #coding: utf-8
 import requests
 import json
+import webbrowser
 from lxml import etree
 from jinja2 import Template
 plantilla = open("plantilla.html","r")
-capitales = ['Almeria','Cadiz','Cordoba','Huelva','Jaen','Sevilla']
+capitales = ['Almeria','Cadiz','Cordoba','Huelva','Jaen','Malaga','Sevilla','Ubeda']
 
 html = ''
 for linea in plantilla:
@@ -12,81 +13,36 @@ for linea in plantilla:
 
 plantilla = Template(html)
 
-pasar = True
-
 lista_temp_min = []
 lista_temp_max = []
 lista_redon = []
 lista_direccion = []
 
-def orientacion(direccion):
-	"""Función que calcula la dirección de la que procede el viento"""
-	for grados in str(direccion):
-		if direccion >= 337 and direccion < 22.5:
-			return 'N'
-		elif direccion >= 22.5 and direccion < 67.5:
-			return 'NE'
-		elif direccion >= 67.5 and direccion < 112.5:
-			return 'E'
-		elif direccion >= 112.5 and direccion < 157.5:
-			return 'SE'
-		elif direccion >= 157.5 and direccion < 202.5:
-			return 'S'
-		elif direccion >= 202.5 and direccion < 247.5:
-			return 'SO'
-		elif direccion >= 247.5 and direccion < 292.5:
-			return 'O'
-		elif direccion >= 292.5 and direccion < 337.5:
-			return 'NO'
-
 for capital in capitales:
-	valores = {'q': '%s,spain' % capital,'mode': 'xml','units': 'metric','lang': 'es'}
+	valores = {'q': '%s' % capital,'mode': 'xml','units': 'metric','lang': 'es'}
 	resultado = requests.get('http://api.openweathermap.org/data/2.5/weather?',params=valores)
 	raiz = etree.fromstring(resultado.text.encode("utf-8"))
 
 	city = raiz.find("city")
 	ciudad = city.attrib["name"]
 	temper_min = raiz.find("temperature")
-	temp_min = temper_min.attrib["min"]
+	temp_min = round(float(temper_min.attrib["min"]),1)
 	temper_max = raiz.find("temperature")
-	temp_max = temper_max.attrib["max"]
-	viento_valor = raiz.find("wind/speed")
-	viento_v = viento_valor.attrib["value"]
-	viento_codigo = raiz.find("wind/direction")
-	viento_c  = viento_codigo.attrib["code"]
-	print ciudad
-	print temp_min
-	print temp_max
-	print viento_v
-	print viento_c
-
-	#print (resultado.text)
-
-	#if dicc == {u'message': u'Error: Not found city', u'cod': u'404'}:
-	#	pasar = False
-	#	temp_min = ' '
-	#	temp_max = ' '
-	#	redon = ' '
-	#	direccion = ' '
+	temp_max = round(float(temper_max.attrib["max"]),1)
+	velocidad = raiz.find("wind/speed")
+	speed = velocidad.attrib["value"]
+	orientacion = raiz.find("wind/direction")
+	direccion = orientacion.attrib["code"]
+	
+	lista_temp_min.append(temp_min)
+	lista_temp_max.append(temp_max)
+	lista_redon.append(speed)
+	lista_direccion.append(direccion)
 
 
-	#if pasar == True:
-	#	temp_min = dicc["main"]["temp_min"] - 273
-	#	temp_max = dicc["main"]["temp_max"] - 273
-	#	viento = round(dicc["wind"]["speed"] * 1.61,2)
-	#	redon = round(viento, 2)
-	#	direccion = dicc["wind"]["deg"]
-	#	direccion = orientacion(direccion)
-	#	lista_temp_min.append(temp_min)
-	#	lista_temp_max.append(temp_max)
-	#	lista_redon.append(redon)
-	#	lista_direccion.append(direccion)
+plantilla = plantilla.render(capitales=capitales,temp_min=lista_temp_min,temp_max=lista_temp_max,speed=lista_redon,direccion_viento=lista_direccion)
 
-#	pasar = True
+resultado=open('salida.html','w')
+resultado.write(plantilla)
 
-#plantilla = plantilla.render(capitales=capitales,temp_min=lista_temp_min,temp_max=lista_temp_max,speed=lista_redon,direccion_viento=lista_direccion)
-
-#resultado=open('salida.html','w')
-#resultado.write(plantilla)
-
-#webbrowser.open("salida.html")
+webbrowser.open("salida.html")
